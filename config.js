8faa6586fb72f2d600cddb5963ffe8719a05f748
JelleWie <1545223+jellewie@users.noreply.github.com>
2020-08-11 09:11:36 +0200

    More real (tested) settings

--- config.js
+++ config.js
@@ -1,57 +1,53 @@
 module.exports = {
-	"fileName": "spiral.gcode",
-	"startCode": `;FLAVOR:Marlin
-;TIME:4756
-;Filament used: 2.42744m
-;Layer height: 0.2
-;MINX:75.826
-;MINY:84.487
-;MINZ:0.2
-;MAXX:137.023
-;MAXY:142.988
-;MAXZ:26.2
-;Generated with Cura_SteamEngine 4.2.1
-M82 ;absolute extrusion mode
-M104 S190 ;set extruder temperature
-M140 S60 ;set bed temperature
-
-M190 S60
-M109 S190
-
-M201 X500.00 Y500.00 Z100.00 E5000.00 ;Setup machine max acceleration
-M203 X500.00 Y500.00 Z10.00 E50.00 ;Setup machine max feedrate
-M204 P500.00 R1000.00 T500.00 ;Setup Print/Retract/Travel acceleration
-M205 X8.00 Y8.00 Z0.40 E5.00 ;Setup Jerk
-M220 S100 ;Reset Feedrate
-M221 S100 ;Reset Flowrate
-
-G28 ;Home
-
-G92 E0 ;Reset Extruder
-G1 Z2.0 F3000 ;Move Z Axis up
-G1 X10.1 Y20 Z0.28 F5000.0 ;Move to start position
-G1 X10.1 Y200.0 Z0.28 F1500.0 E15 ;Draw the first line
-G1 X10.4 Y200.0 Z0.28 F5000.0 ;Move to side a little
-G1 X10.4 Y20 Z0.28 F1500.0 E30 ;Draw the second line
-G92 E0 ;Reset Extruder
-G1 Z2.0 F3000 ;Move Z Axis up
+	"fileName" : "spiral.gcode",
+	
+	"startCode":`;FLAVOR:Marlin
+					;Ender 3 Custom Start G-code
+M104 S200 			;set extruder temp
+M140 S55  			;set bed temp
+M190 S55  			;wait for bed temp (bed will expand so wait for Z homing)
+G28              	;Home
+G29              	;Mesh bed level Z
+G1 X0 Y0 F3000    	;Move nozzle to 0,0
+M109 S200 			;Wait for extruder temp
+;G1 Z7.0 F3000   	;Move the head up a bit
+G92 E0           	;Reset Extruder
+G1 F200 E6       	;Extrude a bit
+G92 E0            	;Reset Extruder
+G1 Z0 F3000       	;Move nozzle down to 0
+G1 X0 Y15 Z0 F3000	;Move to start position
+M117 Printing...
 M141 S28
 G92 E0
-G92 E0
-G1 F2700 E-5
+G1 F2700 E-5`,
+
+	"endCode"  :`	;Ender 3 Custom End G-code
+G91               	;Use Relative Coordinates
+G1 F3000 E-3 Z5    	;Pull filement in a bit (Feedrate = speed) AND move up
+G90               	;Use Absolute Coordinates
+G1 X0 Y230 F3000  	;prepare for part removal, Move to X-0 and Y-max
+M106 S0           	;turn off cooling fan
+M104 S0           	;turn off extruder
+M140 S0            	;turn off bed
+M220 S100          	;Reset Speed factor override percentage to default (100%)
+M221 S100          	;Reset Extrude factor override percentage to default (100%)
+M117 Print done!
+G4 P1             	;Wait for 1 sec
+G1 X234 F1000      	;Automatic shut down
+M84                 ;disable stepper motors
 `,
-	"endCode":"; done",
-	"offsetX": 100,
-	"offsetY": 100,
-	"offsetZ": 0.28,
-	"offsetE": 30,
-	"diameter": 4,
-	"layers": 3,
-	"extrusion": 0.04,
-	"resolution": 15,
+
+	"offsetX"  : 115,
+	"offsetY"  : 115,
+	"offsetZ"  : 0.18,
+	"offsetE"  : 0,
+	"diameter" : 133.1,
+	"layers"   : 10,
+	"extrusion": 0.036,
+	"resolution": 150,
 	"spiralDistance": 0.4,
 	"layerHeight": 0.2,
-	"zHopHeight": 0.4,
-	"zHopRetractAmount": 100,
-	"flipLayers": true,
+	"zHopHeight": 1,
+	"zHopRetractAmount": 6.5,
+	"flipLayers": false,
 }
